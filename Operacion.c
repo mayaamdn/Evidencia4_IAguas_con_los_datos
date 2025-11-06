0c589fac0cf232aa6344b0b811ea554a58d90c30
Victor <A01710270@tec.mx>
2025-11-05 19:31:41 -0600

    Código depurado

--- Operacion.c
+++ Operacion.c
@@ -1,78 +1,22 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2865
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red239\green239\blue239;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c94902\c94902\c94902;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-\f0\fs21\fsmilli10618 \cf0 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #include <stdio.h>\strokec0 \
-\pard\pardeftab720\partightenfactor0
+nt suma(int a, int b){
+return a+b;
+}
+int resta(int a, int b){
+return a-b;
+}
 
-\f1\fs24 \cf0 \strokec2 \
-\
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs21\fsmilli10618 \cf0 #include <stdlib.h>\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\f1\fs24 \cf0 \strokec2 \
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs21\fsmilli10618 \cf0 #include <math.h>\strokec0 \
-\pard\pardeftab720\partightenfactor0
-
-\f1\fs24 \cf0 \strokec2 \
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs21\fsmilli10618 \cf0 \cb3 \strokec2 int main()\{\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 int num1 = 12;\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 int num2 = 3;\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 int op1 = suma(num1,num2);\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 printf("%i",op1);\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 int op2 = resta(num1,num2);\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 printf("%i",op2);\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 int op3 = multi(num1,num2);\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 printf("%i",op3);\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 return 0;\cb1 \strokec0 \
-
-\fs24 \strokec2 \
-
-\fs21\fsmilli10618 \cb3 \}
-\fs24 \cb3 \strokec2 \
-\pard\pardeftab720\partightenfactor0
-
-\f1 \cf0 \cb1 \
-\pard\pardeftab720\partightenfactor0
-
-\f0 \cf0 \cb3 \
-\
+int main(){
+int num1 = 12;
+int num2 = 3;
+int op1 = suma(num1,num2);
+printf("%i",op1);
+int op2 = resta(num1,num2);
+printf("%i",op2);
+int op3 = multi(num1,num2);
+printf("%i",op3);
+return 0;
 }
